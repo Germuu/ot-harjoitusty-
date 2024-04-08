148c20d8ac2fb69a245cc73f7e5da0e66b9d45c6
Germuu <jeremyyamajako@gmail.com>
2024-04-09 02:30:49 +0300

    created recipe service and corrected user_repository to support needed actions

--- recipe-app/src/repositories/user_repository.py
+++ recipe-app/src/repositories/user_repository.py
@@ -1,12 +1,33 @@
+from entities.user import User
 from database_connection import get_database_connection
+import hashlib
 
 class UserRepository:
     def __init__(self, connection):
         self._connection = connection
     
-    def register(username,password):
-        
-    
+    def add_user(self, user):
+        hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
+        query = "INSERT INTO users (username, password) VALUES (?, ?)"
+        self._connection.execute(query, (user.username, hashed_password))
+        self._connection.commit()
 
-    
+    def retrieve_all(self):
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        return [User(row["username"], row["password"]) for row in rows]
 
+    def find_by_username(self, username):
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        row = cursor.fetchone()
+        return User(row["username"], row["password"]) if row else None
+
+    def delete_all(self):
+        query = "DELETE FROM users"
+        self._connection.execute(query)
+        self._connection.commit()
+
+
+user_repository = UserRepository(get_database_connection())
