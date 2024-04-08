e5f0172cd46c0272100722815f3c5c1f30efa4ce
Germuu <jeremyyamajako@gmail.com>
2024-04-09 00:36:09 +0300

    initial setup for database, and optimized structure of directories

---
+++ recipe-app/src/repositories/user_repository.py
@@ -0,0 +1,12 @@
+from database_connection import get_database_connection
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+    
+    def register(username,password):
+        
+    
+
+    
+
